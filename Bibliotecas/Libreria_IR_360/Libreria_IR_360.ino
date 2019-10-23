8738b4161e25943a85277ec9a57b043724d201be
Hiram Castillo <hiramcastillo36@gmail.com>
2019-10-23 16:41:02 -0600

    V 0.1

--- Bibliotecas/Libreria_IR_360/Libreria_IR_360.ino
+++ Bibliotecas/Libreria_IR_360/Libreria_IR_360.ino
@@ -1,3 +1,18 @@
+/*************************************************************************/
+/*                                                                       */
+/*                                                                       */
+/****************************CHABOTS ROBOTNIK*****************************/
+/*                                                                       */
+/*Libreria IR Locator 360 ModernRobotics V 1.0                           */
+/*S.L.P MEXICO                           TIME:10:37:40 PM DATE:10/14/2019*/
+/*************************************************************************/
+
+#include <Wire.h>
+
+void inicializarir(){
+  
+}
+
 void setup() {
   // put your setup code here, to run once:
 
