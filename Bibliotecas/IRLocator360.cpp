5dd2229ac3215e0a178ccde590eecef07b329698
Hiram Castillo <hiramcastillo36@gmail.com>
2019-10-26 14:36:09 -0600

    .

--- Bibliotecas/IRLocator360.cpp
+++ Bibliotecas/IRLocator360.cpp
@@ -3,8 +3,26 @@
 /*                                                                       */
 /****************************CHABOTS ROBOTNIK*****************************/
 /*                                                                       */
+/*Libreria IR Locator 360 ModernRobotics V 3.2                           */
+/*https://modernroboticsinc.com/product/ir-locator-360/                  */
 /*S.L.P MEXICO                           TIME:10:37:40 PM DATE:10/14/2019*/
 /*************************************************************************/
+
+/*
+Direcciones de datos I2C
+  0x00 Sensor Firmware Revision
+  0x01 Manufacturer Code
+  0x02 Sensor ID Code
+  0x03 Not Used
+  0x04 1200 Hz Heading in 5° increments
+  0x05 1200 Hz Signal Strength
+  0x06 600 Hz Heading in 5° increments
+  0x07 600 Hz Signal Strength
+Direccion I2C Arduino 7-Bit
+  0x0E
+Direccion I2c 8-Bit
+  0x1C
+*/
 
 #include "Arduino.h"
 #include "Wire.h"
