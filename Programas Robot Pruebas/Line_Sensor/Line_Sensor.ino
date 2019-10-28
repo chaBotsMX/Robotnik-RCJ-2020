ed32afa46c18d53e47b9697f621fb0930b623e0e
Hiram Castillo <hiramcastillo36@gmail.com>
2019-10-27 23:30:45 -0600

    Update Line_Sensor.ino

--- Programas Robot Pruebas/Line_Sensor/Line_Sensor.ino
+++ Programas Robot Pruebas/Line_Sensor/Line_Sensor.ino
@@ -1,25 +1,17 @@
 #include <Adafruit_NeoPixel.h>
-#define PIN        8 // On Trinket or Gemma, suggest changing this to 1
-#define NUMPIXELS 3 // Popular NeoPixel ring size
+#define PIN        8 
+#define NUMPIXELS 3 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600);
-pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  
-  
-  
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)  
   for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
-
-     //pixels.Color(0,255,0);// takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(100,   0 , 100));
-
-
-      pixels.show();   // Send the updated pixel colors to the hardware.
-
+     //pixels.Color(0,255,0);
+      pixels.setPixelColor(i, pixels.Color(100,   0 , 100));
+      pixels.show();   
   }
-  }
+}
 
 void loop() {
   Serial.print(analogRead(A8)>700);
