44899f23a1b4a3ab195f6c3a005007335cda6be5
Hiram Castillo <hiramcastillo36@gmail.com>
2019-11-28 18:03:21 -0600

    Update Line_Sensor.ino

--- Programas Robot Pruebas/Line_Sensor/Line_Sensor.ino
+++ Programas Robot Pruebas/Line_Sensor/Line_Sensor.ino
@@ -11,28 +11,23 @@ Adafruit_NeoPixel pixel(NUMPIXELS, PIND, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixe(NUMPIXELS, PINT, NEO_GRB + NEO_KHZ800);
 
 
-
-
 void setup() {
   Serial.begin(9600);
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)  
+  pixels.begin();   
   pixel.begin();
   pixe.begin();
   pixels.clear();
   pixel.clear();
-pixe.clear();
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
-
-//     pixels.Color(i,0,55,55);// takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
+  pixe.clear();
+  for(int i=0; i<NUMPIXELS; i++) { 
+    //pixels.Color(i,0,55,55);// takes RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(50, 0, 50));
     pixel.setPixelColor(i, pixel.Color(50,0,50));
     pixe.setPixelColor(i,pixe.Color(50,0,50));
     pixels.show();   // Send the updated pixel colors to the hardware.
-pixel.show();
-pixe.show();
+	pixel.show();
+	pixe.show();
   }
-
 }
 
 byte portsS1[]={A0,A1,A2};
