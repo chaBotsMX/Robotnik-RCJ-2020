185e90e9574ffd06eeadacbfbfa7c65ff4c878ec
Hiram Castillo <hiramcastillo36@gmail.com>
2019-12-25 20:00:56 -0600

    Update PID.cpp

--- Programas Robot Pruebas/Pruebas C++/PID.cpp
+++ Programas Robot Pruebas/Pruebas C++/PID.cpp
@@ -8,20 +8,20 @@ using namespace std;
 
 
 int main(){
-    int a=80;
-    int b=20;
-    bool x;
-    int C=10;
-    C>180 ? x=1 : x=0;
-    C>180 ? C=360.00-C : C=C;
+    int a=80; //Intencidad actual de la pelota
+    int b=20; // Intensidad constante de la pelota
+    bool x; // Bandera
+    int C=235; // Angulo actual del sensor IR
+    C>180 ? x=1 : x=0; //Bandera
+    C>180 ? C=360.00-C : C=C; // Mayor o menor a 180
 
     //((a*a)+(b*b))-2(a)*(b)*cos(C))
     while(a!=b&&C>0){
-        double c= sqrt(((a*a)+(b*b))-2*(a*b)*cos(C*M_PI/180.00));
-        double d = asin(((a*sin(C*M_PI/180.00))/c))*180.00/M_PI;
+        double c= sqrt(((a*a)+(b*b))-2*(a*b)*cos(C*M_PI/180.00));  //Calculo del lado del angulo IR
+        double d = asin(((a*sin(C*M_PI/180.00))/c))*180.00/M_PI; //Calculo de angulo restante
         b=b+1;
         C=C-5;
-        if(!x)
+        if(!x) // Un angulo         Otro angulo         A donde se va a mover
             cout<<180.00-C-d<<"\t"<<d<<"\t"<<180-(180.00-C-d)<<"\n";
         else
             cout<<180.00-C-d<<"\t"<<d<<"\t"<<180+(180.00-C-d)<<"\n";
