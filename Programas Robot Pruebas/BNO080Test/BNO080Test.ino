b0698659b8fb47d338185646ac8d4e96fcc5b0d6
Hiram Castillo <hiramcastillo36@gmail.com>
2020-01-13 10:35:21 -0600

    IMU

---
+++ Programas Robot Pruebas/BNO080Test/BNO080Test.ino
@@ -0,0 +1,69 @@
+#include "SparkFun_BNO080_Arduino_Library.h"
+#include <Wire.h>
+
+double targetRotation=0.0;
+BNO080 myIMU;
+
+double getFilter(double rot){
+  double rt=rot;
+  if(rt<=180)
+    return rt/180.0;
+  return (rt-360)/180.0;
+}
+
+double error(double rot){
+    double err=360.00-rot;
+    if(err>180.00)
+      err=err-360.00;
+    return err;    
+}
+
+double getRawRotation(){
+  myIMU.dataAvailable();
+  float x = myIMU.getQuatI();
+  float y = myIMU.getQuatJ();
+  float z = myIMU.getQuatK();
+  float w = myIMU.getQuatReal();
+  float quatRadianAccuracy = myIMU.getQuatRadianAccuracy();
+  double siny_cosp = 2.0 * (w * z + x * y);
+  double cosy_cosp = 1.0 - 2.0 * (y * y + z * z);
+  return atan2(siny_cosp, cosy_cosp)*180.0/3.141592653;
+}
+
+double getRotation(){
+  // Leer la rotacion en cuanto al Norte Magnetico del IMU
+  double rot = getRawRotation();
+  
+  // Obtener la rotacion relativa al frente inicial (rotacion "0")
+  double rt=rot-targetRotation;
+
+  // Condiciones para asegurarnos de que el valor de rotacion
+  // regresado (como resultado) por la funcion sea valido 
+  // (entre 0 y 360).
+  if(rt<0)
+    rt+=360;
+  if(rt>360)
+    rt-=360;
+  // Regresar la rotacion actual del robot
+  return rt;
+  
+}
+
+void setup() {
+  Serial.begin(9600);  
+  Wire.begin();
+  
+  myIMU.begin();
+  myIMU.enableRotationVector(50);
+  
+  while(millis()<600){
+    targetRotation=getRawRotation();
+  }
+     
+}
+
+void loop() {
+  Serial.println(error(getRotation()));
+  //Serial.println(getFilter(360.00-getRotation()));
+  //Serial.println(360.00-getRotation());
+}
---
+++ Programas Robot Pruebas/Calibracion/Calibracion.ino
@@ -0,0 +1,35 @@
+#include "SparkFun_BNO080_Arduino_Library.h"
+#include <Wire.h>
+#include <DueFlashStorage.h>
+
+const int magx = 10;
+const int magy = 5;
+
+double targetRotation=0.0;
+BNO080 myIMU;
+DueFlashStorage dueFlashStorage;
+
+
+void setup() {
+  Serial.begin(9600);  
+  Wire.begin();
+  myIMU.begin();
+  myIMU.enableMagnetometer(50);
+  delay(20);
+}
+
+
+
+void loop() {
+  myIMU.dataAvailable();
+  float x = myIMU.getMagX();
+  float y = myIMU.getMagY();
+  float z = myIMU.getMagZ();
+  float angle = atan2(x-magx, y-magy)*180.00/M_PI;
+  Serial.print(angle);
+  Serial.print("\t");
+  Serial.print(x);
+  Serial.print("\t");
+  Serial.print(y);
+  Serial.print("\n");
+}
