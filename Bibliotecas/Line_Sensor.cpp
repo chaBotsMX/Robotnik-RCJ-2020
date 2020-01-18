2dc1abb69cd49e01ca30840a99e3ac9295720b7d
Hiram Castillo <hiramcastillo36@gmail.com>
2020-01-17 18:38:01 -0600

    LS

--- Bibliotecas/Line_Sensor.cpp
+++ Bibliotecas/Line_Sensor.cpp
@@ -10,28 +10,25 @@
 #include "Arduino.h"
 #include "Line_Sensor.h"
 
-Line_Sensor::Line_Sensor(byte port[], int umbral) {
-	for (int i = 0; i < 3; i++)
+Line_Sensor::Line_Sensor(int port[], int umbral) {
+  for (int i = 0; i < 3; i++)
 		_port[i] = port[i];
 	_umbral = umbral;
 }
 
 bool Line_Sensor::isLine() {
+
 	for (int i = 0; i < 3; i++)
 		if (analogRead(_port[i]) >= _umbral)
 			return true;
 	return false;
 }
-/*
-void Line_Sensor::setColor(byte r, byte g, byte b) {
-	pixels.Color(_r, _g, _b);
-	pixels.show();   
-}
 
-void Line_Sensor::itsMomentToFun(byte r, byte g, byte b) {
-	for(int i=0; i<numPixels; i++){ 
-		pixels.setPixelColor(i, pixels.Color(_r, _b, _b));
-		pixels.show();
-	}
+void Line_Sensor::SensorValue(){
+	Serial.print(analogRead(_port[0]));
+	Serial.print("\t");
+	Serial.print(analogRead(_port[1]));
+	Serial.print("\t");
+	Serial.print(analogRead(_port[2]));
+	Serial.print("\n");
 }
-*/
