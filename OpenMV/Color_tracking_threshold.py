e36da9bd37a9980cc424d9d908de0d4c78349e1e
Hiram Castillo <hiramcastillo36@gmail.com>
2020-02-02 23:07:23 -0600

    OpenMV

--- OpenMV/Color_tracking_threshold.py
+++ OpenMV/Color_tracking_threshold.py
@@ -3,7 +3,6 @@
 # This example shows off single color automatic RGB565 color tracking using the OpenMV Cam.
 
 import sensor, image, time
-import pyb
 print("Letting auto algorithms run. Don't put anything in front of the camera!")
 
 sensor.reset()
@@ -13,10 +12,9 @@ sensor.skip_frames(time = 500)
 sensor.set_auto_gain(False) # must be turned off for color tracking
 sensor.set_auto_whitebal(False) # must be turned off for color tracking
 clock = time.clock()
-led = pyb.LED(3)
 
 # Capture the color thresholds for whatever was in the center of the image.
-r = [(320//2)-(50//2), (240//2)-(50//2), 80, 80] # 50x50 center of QVGA.
+r = [(320//2)-(50//2), (240//2)-(50//2), 60, 60] # 50x50 center of QVGA.
 
 print("Auto algorithms done. Hold the object you want to track in front of the camera in the box.")
 print("MAKE SURE THE COLOR OF THE OBJECT YOU WANT TO TRACK IS FULLY ENCLOSED BY THE BOX!")
@@ -25,8 +23,8 @@ for i in range(60):
     img.draw_rectangle(r)
 
 print("Learning thresholds...")
-threshold = [50, 50, 0, 0, 0, 0] # Middle L, A, B values.
-for i in range(80):
+#threshold = [50, 50, 0, 0, 0, 0] # Middle L, A, B values.
+""""for i in range(60):
     img = sensor.snapshot()
     hist = img.get_histogram(roi=r)
     lo = hist.get_percentile(0.01) # Get the CDF of the histogram at the 1% range (ADJUST AS NECESSARY)!
@@ -38,10 +36,11 @@ for i in range(80):
     threshold[3] = (threshold[3] + hi.a_value()) // 2
     threshold[4] = (threshold[4] + lo.b_value()) // 2
     threshold[5] = (threshold[5] + hi.b_value()) // 2
-    for blob in img.find_blobs([threshold], pixels_threshold=200, area_threshold=200, merge=True, margin=20):
+    for blob in img.find_blobs([threshold], pixels_threshold=100, area_threshold=100, merge=True, margin=20):
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
-        img.draw_rectangle(r)
+        img.draw_rectangle(r)"""
+threshold = [76, 85, -30, -23, -21, -15]
 
 print("Thresholds learned...")
 print("Tracking colors...")
@@ -51,12 +50,9 @@ print(threshold)
 while(True):
     clock.tick()
     img = sensor.snapshot()
-    led.off()
     for blob in img.find_blobs([threshold], pixels_threshold=200, area_threshold=200, merge=True, margin=20):
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
         print(((blob.rotation()*180.00)/3.1415))
-        led.on()
-
 
 
