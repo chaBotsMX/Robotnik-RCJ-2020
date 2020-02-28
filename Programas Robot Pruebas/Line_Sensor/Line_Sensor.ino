2e392b287ce2c5e55b22b5fbf7bd8234d198bdb4
Hiram Castillo <hiramcastillo36@gmail.com>
2020-02-27 18:52:07 -0600

    Sensor Linea

--- Programas Robot Pruebas/Line_Sensor/Line_Sensor.ino
+++ Programas Robot Pruebas/Line_Sensor/Line_Sensor.ino
@@ -9,6 +9,7 @@
 #define intensidad 50
 int sensorX[]={1,0,-1,0};
 int sensorY[]={0,1,0,-1};
+int mov[]={180,270,0,90};
 bool sensor[4];
 int contador=0;
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_RGB + NEO_KHZ800);
@@ -45,6 +46,7 @@ int angulo(){
     sensor[1]>0 ?  sumay = sumay + sensorY[1] : sumax= sumax + sensorX[1]; // 90
     sensor[2]>0 ?  sumax = sumax + sensorX[2] : sumay= sumay + sensorY[2]; // 180
     sensor[3]>0 ?  sumay = sumay + sensorY[3] : sumax= sumax + sensorX[3]; // 270
+    
     if(sumax!=0&&sumay!=0){
         angle=atan((sumay/sumax))*180.00/M_PI;
         if(angle<0){
@@ -53,15 +55,28 @@ int angulo(){
             else
                 angle=angle+180;
         }
-        else {
-           angle=angle+180;
-      }
+        else{
+           if(sumax!=-1&&sumay!=-1)angle=angle+180;
+        }
     }
     else{
-        if(sumax==0&&sumay==0)
-            angle=180;
-       
-      
+        if(sumax==0&&sumay==0){
+            return -1;
+        }
+        else{
+            for(int i=0; i<=3; i++){
+                if(sensor[i]){
+                    angle=angle+mov[i];
+                    contador++;
+                }
+            }
+        }
+    }
+    if(contador==3){
+        if(angle==360||angle==540)
+            angle=angle-360;
+        else
+            angle=angle-180;
     }
     return angle;
 }
@@ -101,7 +116,7 @@ void loop() {
   sensor[0]=isLine(3,5);//bien
   sensor[1]=isLine(0,2);
   sensor[2]=isLine(9,11);
-   sensor[3]=isLine(6,8);
+  sensor[3]=isLine(6,8);
   Serial.print(sensor[0]); //ENFRENTE
   Serial.print("\t");
   Serial.print(sensor[1]); //DERECHA
@@ -110,6 +125,6 @@ void loop() {
   Serial.print("\t");
   Serial.print(sensor[3]); //IZQUIERDA
   Serial.print("\t");
-  Serial.print(angulo2());
+  Serial.print(angulo());
   Serial.print("\n");
 }
